3c33c66fd39caf86d48e198ab336fafce6129a5f
paryuyu <107971288+paryuyu@users.noreply.github.com>
2022-12-12 10:24:28 +0900

    main_backdrop수정

--- components/layout/Main.tsx
+++ components/layout/Main.tsx
@@ -1,48 +1,45 @@
 import { Backdrop, Card, CardContent, CardMedia, CircularProgress, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import { useEffect, useState , useContext} from "react";
-import { AccomodationData } from "../../lib/model/accomodation";
+import { useEffect, useState, useContext } from "react";
+import accomodation, { AccomodationData } from "../../lib/model/accomodation";
 import { useRouter } from "next/router";
 import MainCard from "../ui/mainCard/mainCard";
 import Head from "next/head";
 import { CategoryCtx } from "../../context/category-context";
 import { BackDropContext } from "../../pages/_app";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import dbConnect from "../../lib/db_connect";
 
-export default function MainPage() {
+export default function MainPage({}) {
   const [mainData, setMainData] = useState<AccomodationData[]>([]);
   const router = useRouter();
-  const ctx= useContext(CategoryCtx);
-  const backCtx =useContext(BackDropContext)
-  async function MainReq() {
+  const ctx = useContext(CategoryCtx);
+  const backCtx = useContext(BackDropContext);
 
-    let res = await fetch('/api/main/find?publish=true&category='+ctx.category);
+  async function MainReq() {
+    let res = await fetch('/api/main/find?publish=true&category=' + ctx.category);
     let json = await res.json();
     setMainData(json.data)
-    console.log(json)
-    if(json.result){
-      backCtx.setBackDrop(false)
-    } 
   };
 
   useEffect(() => {
-    backCtx.setBackDrop(true)
     MainReq()
   }, [ctx.category])
 
 
   return (<>
-  <Head>
-    <title>메인페이지</title>
-  </Head>
-{mainData && mainData.length>0 ?
+    <Head>
+      <title>메인페이지</title>
+    </Head>
+    {mainData && mainData.length > 0 ?
 
-    <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1.5, flexWrap: 'wrap', justifyContent:'center',  pl: 2, mt:2}}>
+      <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1.5, flexWrap: 'wrap', justifyContent: 'center', pl: 2, mt: 2 }}>
 
-    { mainData.map((one,index) => <MainCard item={one} key={index}/>)}
+        {mainData.map((one, index) => <MainCard item={one} key={index} />)}
 
-    </Box>:<Typography>해당 카테고리에 존재하는 데이터가 없습니다.</Typography>
+      </Box> : <Typography>해당 카테고리에 존재하는 데이터가 없습니다.</Typography>
 
-}
+    }
   </>);
 }
 
